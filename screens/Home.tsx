6973bea922522c3e4044d60ce298722a953068c9
Gustave Nzitunga <112557030+GunziThanzo@users.noreply.github.com>
2024-08-18 22:40:51 +0200

    first commit

---
+++ screens/Home.tsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+import { View, Text, ScrollView } from "react-native";
+import { Category, Transaction } from "../types";
+import { useSQLiteContext } from "expo-sqlite/next";
+import TransactionsList from "../components/TransactionsList";
+
+export default function Home() {
+
+    const [categories, setCategories] = React.useState<Category[]>([]);
+    const [transactions, setTransactions] = React.useState<Transaction[]>([]);   
+
+
+    const db = useSQLiteContext();
+
+    React.useEffect(() => {
+        db.withTransactionAsync(async () => {
+            await getData();
+        })
+    }, [db])
+
+    async function getData() {
+        const result = await db.getAllAsync<Transaction>(`SELECT * FROM Transactions ORDER BY date DESC;`);
+        setTransactions(result);
+
+        const categorieResult = await db.getAllAsync<Category>(`SELECT * FROM Categories;`);
+        setCategories(categorieResult);
+
+    }
+
+    async function deleteTransaction(id: number) {
+        db.withTransactionAsync(async () => {
+            await db.runAsync(`DELETE FROM Transactions WHERE id = ?;`, [id])
+            await getData(); 
+        })
+    }
+
+
+
+    return (
+        <ScrollView
+            contentContainerStyle={{
+                padding: 15,
+                paddingVertical: 80
+            }}
+        >
+            <TransactionsList 
+                categories={categories}
+                transactions={transactions}
+                deleteTransaction={deleteTransaction}
+            />
+        </ScrollView>
+    )
+}
+
+
+
